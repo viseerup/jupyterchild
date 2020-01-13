b90ce83b6aa9fdc7a759cb7590ef60d69c861d4a
Vivian Machindano <vivianmachindano@Vivians-MacBook-Pro.local>
2020-01-13 11:24:54 +0100

    created a post app with models and display posts in the index page. created a marketing app for subscribe users.

---
+++ posts/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+
+
+User = get_user_model()
+
+
+
+class Author(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	profile_picture = models.ImageField()
+
+	def __str__(self):
+		return self.user.username
+
+
+
+class Category(models.Model):
+	title = models.CharField(max_length=20)
+
+	def __str__(self):
+		return self.title
+
+
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	overview = models.TextField()
+	timestamp = models.DateTimeField(auto_now_add=True)
+	author = models.ForeignKey(Author, on_delete=models.CASCADE)
+	coment_cout = models.IntegerField(default=0)
+	thumbnail = models.ImageField()
+	categories = models.ManyToManyField(Category)
+	featured = models.BooleanField()
+	
+
+	def __str__(self):
+		return self.title
