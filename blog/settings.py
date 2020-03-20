e520e2c0642cb11bb7b817a329a0bb55d91ada1a
Vivian Machindano <vivianmachindano@Vivians-MacBook-Pro.local>
2020-03-20 14:11:55 +0100

    edit the settings

--- blog/settings.py
+++ blog/settings.py
@@ -94,15 +94,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd82p7u8flmq16',
-        'USER': 'eagxslffptvebe',
-        'PASSWORD': '64bcff6c18cdb282f8b96c2860359a27bf3d97a0010455da8c615b75e49ed185',
-        'HOST': 'ec2-18-235-97-230.compute-1.amazonaws.com',
-        'PORT': '5432',
-    },
+
   DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.sqlite3',
