702061b15f0b3b3c67342aa753603c4fe1a85bbd
Vivian Machindano <vivianmachindano@Vivians-MacBook-Pro.local>
2020-03-20 15:23:08 +0100

    change settings

--- blog/settings.py
+++ blog/settings.py
@@ -102,6 +102,9 @@ WSGI_APPLICATION = 'blog.wsgi.application'
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
