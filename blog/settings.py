4f7d0a8d82c10949c5a83ca1b8d974e485e7685c
Vivian Machindano <vivianmachindano@Vivians-MacBook-Pro.local>
2020-04-07 13:47:47 +0200

    created menus link and correct the sintax error

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jupiterchildart.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -96,12 +96,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
